2251a84fe703710ed1b6b42650898e061a002903
Michael Kennedy <mikeckennedy@gmail.com>
2018-09-11 12:43:31 -0700

    CPU bound operation on threads -- no improvement.

--- src/05-threads/cpu_attempt/compute_threaded.py
+++ src/05-threads/cpu_attempt/compute_threaded.py
@@ -1,5 +1,7 @@
 import datetime
 import math
+from threading import Thread
+import multiprocessing
 
 
 def main():
@@ -7,7 +9,20 @@ def main():
 
     t0 = datetime.datetime.now()
 
-    do_math(num=30000000)
+    # do_math(num=30000000)
+    print("Doing math on {:,} processors.".format(multiprocessing.cpu_count()))
+
+    processor_count = multiprocessing.cpu_count()
+    threads = []
+    for n in range(1, processor_count + 1):
+        threads.append(Thread(target=do_math,
+                              args=(30_000_000 * (n - 1) / processor_count,
+                                    30_000_000 * n / processor_count),
+                              daemon=True)
+                       )
+
+    [t.start() for t in threads]
+    [t.join() for t in threads]
 
     dt = datetime.datetime.now() - t0
     print("Done in {:,.2f} sec.".format(dt.total_seconds()))
@@ -18,7 +33,7 @@ def do_math(start=0, num=10):
     k_sq = 1000 * 1000
     while pos < num:
         pos += 1
-        dist = math.sqrt((pos - k_sq)*(pos - k_sq))
+        math.sqrt((pos - k_sq) * (pos - k_sq))
 
 
 if __name__ == '__main__':
