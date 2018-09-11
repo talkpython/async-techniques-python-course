054a71893cce9eee0c93c532e7cb8b3cdaf7ac69
Michael Kennedy <mikeckennedy@gmail.com>
2018-09-11 14:27:14 -0700

    Fine grained thread safety "working"!

--- src/06-thread-safety/safe_bank_fine_grained.py
+++ src/06-thread-safety/safe_bank_fine_grained.py
@@ -8,6 +8,7 @@ from typing import List
 class Account:
     def __init__(self, balance=0):
         self.balance = balance
+        self.lock = RLock()
 
 
 def main():
@@ -37,11 +38,13 @@ def main():
 
 
 def do_bank_stuff(accounts, total):
-    for _ in range(1, 10_000):
+    for n in range(1, 10_000):
         a1, a2 = get_two_accounts(accounts)
         amount = random.randint(1, 100)
         do_transfer(a1, a2, amount)
-        validate_bank(accounts, total, quiet=True)
+
+        if n % 1000:
+            validate_bank(accounts, total, quiet=True)
 
 
 def create_accounts() -> List[Account]:
@@ -51,30 +54,27 @@ def create_accounts() -> List[Account]:
     ]
 
 
-transfer_lock = RLock()
-
-
 def do_transfer(from_account: Account, to_account: Account, amount: int):
     if from_account.balance < amount:
         return
 
-    # Not so good:
-    # transfer_lock.acquire()
-    #
-    # from_account.balance -= amount
-    # time.sleep(.000)
-    # to_account.balance += amount
-    #
-    # transfer_lock.release()
+    lock1, lock2 = (
+        (from_account.lock, to_account.lock)
+        if id(from_account) < id(to_account)
+        else (to_account.lock, from_account.lock)
+    )
 
-    # good!
-    with transfer_lock:
-        from_account.balance -= amount
-        time.sleep(.000)
-        to_account.balance += amount
+    with lock1:
+        with lock2:
+            from_account.balance -= amount
+            time.sleep(.000)
+            to_account.balance += amount
 
 
 def validate_bank(accounts: List[Account], total: int, quiet=False):
+    accounts = sorted(accounts, key=lambda a: id(a))
+    [a.lock.acquire() for a in accounts]
+
     current = sum(a.balance for a in accounts)
     if current != total:
         print("ERROR: Inconsistent account balance: ${:,} vs ${:,}".format(
@@ -83,6 +83,8 @@ def validate_bank(accounts: List[Account], total: int, quiet=False):
     elif not quiet:
         print("All good: Consistent account balance: ${:,}".format(
             total), flush=True)
+
+    [a.lock.release() for a in accounts]
 
 
 def get_two_accounts(accounts):
