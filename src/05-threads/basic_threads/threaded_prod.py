77bbf62f5976fdb8e1f476538b1d7a6c10409f73
Michael Kennedy <mikeckennedy@gmail.com>
2018-09-11 12:32:53 -0700

    Producer consumer, now with cancellation.

--- src/05-threads/basic_threads/threaded_prod.py
+++ src/05-threads/basic_threads/threaded_prod.py
@@ -12,17 +12,28 @@ def main():
     data = []
 
     threads = [
-        threading.Thread(target=generate_data, args=(20, data)),
-        threading.Thread(target=generate_data, args=(20, data)),
-        threading.Thread(target=process_data, args=(40, data)),
+        threading.Thread(target=generate_data, args=(20, data), daemon=True),
+        threading.Thread(target=generate_data, args=(20, data), daemon=True),
+        threading.Thread(target=process_data, args=(40, data), daemon=True),
     ]
+    abort_thread = threading.Thread(target=check_cancel, daemon=True)
+    abort_thread.start()
 
     [t.start() for t in threads]
-    print("Started...")
-    [t.join() for t in threads]
+
+    while any([t.is_alive() for t in threads]):
+        [t.join(.001) for t in threads]
+        if not abort_thread.is_alive():
+            print("Cancelling on your request!", flush=True)
+            break
 
     dt = datetime.datetime.now() - t0
     print(colorama.Fore.WHITE + "App exiting, total time: {:,.2f} sec.".format(dt.total_seconds()), flush=True)
+
+
+def check_cancel():
+    print(colorama.Fore.RED + "Press enter to cancel...", flush=True)
+    input()
 
 
 def generate_data(num: int, data: list):
