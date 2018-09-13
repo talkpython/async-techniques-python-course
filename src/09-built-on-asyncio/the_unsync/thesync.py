413ee93df24191a7d72570b42b8d3b5d0765a84f
Michael Kennedy <mikeckennedy@gmail.com>
2018-09-13 13:37:39 -0700

    Final code for unsync demo.

--- src/09-built-on-asyncio/the_unsync/thesync.py
+++ src/09-built-on-asyncio/the_unsync/thesync.py
@@ -1,3 +1,4 @@
+from unsync import unsync
 import asyncio
 import datetime
 import math
@@ -9,34 +10,34 @@ import requests
 def main():
     t0 = datetime.datetime.now()
 
-    loop = asyncio.get_event_loop()
-
     tasks = [
-        loop.create_task(compute_some()),
-        loop.create_task(compute_some()),
-        loop.create_task(compute_some()),
-        loop.create_task(download_some()),
-        loop.create_task(download_some()),
-        loop.create_task(download_some_more()),
-        loop.create_task(download_some_more()),
-        loop.create_task(wait_some()),
-        loop.create_task(wait_some()),
-        loop.create_task(wait_some()),
-        loop.create_task(wait_some()),
+        compute_some(),
+        compute_some(),
+        compute_some(),
+        download_some(),
+        download_some(),
+        download_some_more(),
+        download_some_more(),
+        wait_some(),
+        wait_some(),
+        wait_some(),
+        wait_some(),
     ]
 
-    loop.run_until_complete(asyncio.gather(*tasks))
+    [t.result() for t in tasks]
 
     dt = datetime.datetime.now() - t0
     print("Synchronous version done in {:,.2f} seconds.".format(dt.total_seconds()))
 
 
-async def compute_some():
+@unsync(cpu_bound=True)
+def compute_some():
     print("Computing...")
     for _ in range(1, 10_000_000):
         math.sqrt(25 ** 25 + .01)
 
 
+@unsync()
 async def download_some():
     print("Downloading...")
     url = 'https://talkpython.fm/episodes/show/174/coming-into-python-from-another-industry-part-2'
@@ -49,7 +50,8 @@ async def download_some():
     print("Downloaded (more) {:,} characters.".format(len(text)))
 
 
-async def download_some_more():
+@unsync()
+def download_some_more():
     print("Downloading more ...")
     url = 'https://pythonbytes.fm/episodes/show/92/will-your-python-be-compiled'
     resp = requests.get(url)
@@ -60,6 +62,7 @@ async def download_some_more():
     print("Downloaded {:,} characters.".format(len(text)))
 
 
+@unsync()
 async def wait_some():
     print("Waiting...")
     for _ in range(1, 1000):
