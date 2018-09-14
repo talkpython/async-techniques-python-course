b1e70ce739b814f1c8982f1bed6c0dbf1be1ab2c
Michael Kennedy <mikeckennedy@gmail.com>
2018-09-14 13:50:01 -0700

    Final code for Cython threading (float version)

--- src/11-cython/perf/compute_cython.py
+++ src/11-cython/perf/compute_cython.py
@@ -7,27 +7,27 @@ import multiprocessing
 def main():
     math_core.do_math(1)
 
-    t0 = datetime.datetime.now()
-
-    # math_core.do_math(num=30_000_000)
     print("Doing math on {:,} processors.".format(multiprocessing.cpu_count()))
 
     processor_count = multiprocessing.cpu_count()
     threads = []
     for n in range(1, processor_count + 1):
         threads.append(Thread(target=math_core.do_math,
-                              args=(300_000 * (n - 1) / processor_count,
-                                    300_000 * n / processor_count),
+                              args=(3_000_000 * (n - 1) / processor_count,
+                                    3_000_000 * n / processor_count),
                               daemon=True)
                        )
 
+    t0 = datetime.datetime.now()
     [t.start() for t in threads]
+
     [t.join() for t in threads]
+    # math_core.do_math(num=300_000)
 
     dt = datetime.datetime.now() - t0
     print("Done in {:,.2f} sec. (factor: {:,.2f}x)".format(
         dt.total_seconds(),
-        .09/dt.total_seconds())
+        0.80 / dt.total_seconds())
     )
 
 
--- src/11-cython/perf/math_core.pyx
+++ src/11-cython/perf/math_core.pyx
@@ -2,9 +2,9 @@ from libc.math cimport sqrt
 
 import cython
 
-def do_math(start: cython.int = 0, num: cython.int = 10):
-    pos: cython.int = start
-    k_sq: cython.int = 1000 * 1000
+def do_math(start: cython.float = 0, num: cython.float = 10):
+    pos: cython.float = start
+    k_sq: cython.float = 1000 * 1000
 
     with nogil:
         while pos < num:
