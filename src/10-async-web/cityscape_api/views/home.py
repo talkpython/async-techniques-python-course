aedbb01c138b66b190f011db11ca232b84e0c148
Michael Kennedy <mikeckennedy@gmail.com>
2018-09-13 17:16:28 -0700

    Starter code for async web chapter.

---
+++ src/10-async-web/cityscape_api/views/home.py
@@ -0,0 +1,15 @@
+import flask
+
+blueprint = flask.blueprints.Blueprint(__name__, __name__)
+
+
+@blueprint.route('/')
+def index():
+    return "Welcome to the city_scape API. " \
+           "Use /api/sun/[zipcode]/[country code (e.g. us)] and" \
+           "/api/weather/[zipcode]/[country code (e.g. us)] for API calls."
+
+
+@blueprint.errorhandler(404)
+def not_found(_):
+    return flask.Response("The page was not found.", status=404)
